4890447e2422d3d258a3e00580aff0e08e6e5c9f
Neo-poznan <ilapravednikov67@gmail.com>
2025-01-20 23:27:46 +0300

    added videocall test with sfu server

--- CRM/static/js/private_call_room.js
+++ CRM/static/js/private_call_room.js
@@ -1,109 +1,48 @@
-const socket = io('http://127.0.0.1:5001', {'transports': ['websocket'], 'query': {'user_id': user_id}});
+const socket = io('http://127.0.0.1:5001', {'transports': ['websocket']});
 
-
-let localPeer;
-
-let localStream;
-
-const localVideo = document.getElementsByClassName('my-video')[0]
-const remoteVideo = document.getElementsByClassName('interlocutor-video')[0]
-
-let offerConstrains;
-
-
-const servers = {
-    iceServers: [
-      { urls: 'stun:stun.l.google.com:19302'},
-    ]
-  };
-
-async function createStream() {
-    localStream = await navigator.mediaDevices.getUserMedia({'video': true, 'audio': true})
-    localVideo.srcObject = localStream
+function generateUniqueSSRC() {
+    return Math.floor(Math.random() * 4294967295);
 }
 
-$('.get-media').click(createStream)
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(stream => {
+        document.getElementsByClassName('my-video')[0].srcObject = stream;
 
-var offerConstraints = {};
+        stream.getTracks().forEach(track => {
+            const rtpParameters = {
+                codecs: [
+                    {
+                        mimeType: 'video/VP8',
+                        payloadType: 96,
+                        clockRate: 90000,
+                        parameters: {
+                            'x-google-start-bitrate': 1000,
+                        },
+                    },
+                ],
+                headerExtensions: [
+                    {
+                        uri: 'urn:ietf:params:rtp-hdrext:sdes:mid',
+                        id: 1,
+                    },
+                    {
+                        uri: 'urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id',
+                        id: 2,
+                    },
+                ],
+                encodings: [
+                    {
+                        ssrc: generateUniqueSSRC(), // Генерация уникального идентификатора для потока
+                        maxBitrate: 1000000,
+                    },
+                ],
+            };
 
-function offerCreationSuccess(desc) {
-    console.log('Успешно создан оффер. Устанавливается локальное описание для локального пира 1. Передается на вторую страницу.')
-    console.log('Offer: ', desc)
-    localPeer.setLocalDescription(desc)
-    socket.emit('created_offer', JSON.stringify({'offer': desc}))
-}
+            socket.emit('broadcast-stream', { kind: track.kind, rtpParameters: rtpParameters });
+        });
+    });
 
-
-function offerCreationError (error) {
-    console.log('Error creation offer', error)
-}
-
-
-function createOffer() {
-    localPeer.createOffer(
-        offerCreationSuccess,
-        offerCreationError,
-        offerConstraints
-    )
-}
-
-function onAddIceCandidateOnLocalPeer(event){
-    if (event.candidate) {
-      console.log("Добавлен ice candidate на локальный пир. Вызов удаленного пира для добавления кандидата на него"+ event.candidate.candidate.replace("\r\n", ""), event.candidate);
-      // вызываем удаленный пир для добавления кандидата
-      socket.emit('ice_candidates', JSON.stringify({'candidate': event.candidate}))
-    }
-  }
-
-
-function onAddRemoteStreamToLocalPeer(event) {
-    console.log("Видео с удаленного пира было добавлено на локальный пир");
-    // remoteVideo.srcObject = event.stream;
-}
-
-  function createOfferOnClick() {
-    console.log("createOffer_click()");
-    localPeer = new RTCPeerConnection(servers); // Создаем RTCPeerConnection
-    console.log('Local peer created')
-    localPeer.onicecandidate = onAddIceCandidateOnLocalPeer;    // Callback-функция для обработки ICE-кандидатов
-    localPeer.onaddstream = onAddRemoteStreamToLocalPeer;          // Callback-функция, вызываемая при появлении медиапотока от дальней стороны. Пока что его нет
-    localPeer.addStream(localStream); // Передадим локальный медиапоток (предполагаем, что он уже получен)
-
-
-    createOffer()
-}
-
-$('.offer').click(createOfferOnClick)
-
-  
-function disconnect() {
-    localPeer.close();
-}
-
-$('.disconnect').click(disconnect)
-
-
-socket.on('ice_candidates', (arg) => {
-    
-    let arg_json = JSON.parse(arg)
-    let arg_data = arg_json.candidate
-    localPeer.addIceCandidate( new RTCIceCandidate(arg_data))
-    console.log('Получено сообщение из сокета. Добавлен кандидат из него на пир.', arg_data)
+socket.on('producer-created', producerId => {
+    console.log('Producer created with ID:', producerId);
 });
 
-socket.on('set_description', (arg) => {
-    let arg_json = JSON.parse(arg)
-    let arg_data = arg_json.description
-    localPeer.setRemoteDescription( new RTCSessionDescription(arg_data))
-    console.log('Принято описание из сокета. Установлено в качестве удаленного.')
-});
-
-
-
-
-
-
-
-
-
- 
--- CRM/static/js/receive.js
+++ CRM/static/js/receive.js
@@ -1,76 +1,39 @@
-const socket = io('http://127.0.0.1:5001', {'transports': ['websocket'], 'query': {'user_id': user_id}});
+const socket = io('http://127.0.0.1:5001', {'transports': ['websocket']});
+   
+const rtpCapabilities = {
+  mediaCodecs: [
+    {
+      kind: 'video',
+      mimeType: 'video/VP8',
+      clockRate: 90000,
+      parameters: {
+        'x-google-start-bitrate': 1000,
+      },
+    },
 
-
-let localPeer;
-
-const remoteVideo = document.getElementById('remote-video')
-
-const servers = {
-  iceServers: [
-    { urls: 'stun:stun.l.google.com:19302' },
-  ]
+  ],
+  headerExtensions: [
+    {
+        uri: 'urn:ietf:params:rtp-hdrext:sdes:mid',
+        id: 1,
+    },
+    {
+        uri: 'urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id',
+        id: 2,
+    },
+],
 };
-
-function OnSuccessAnswerCreation(desc) {  
-  localPeer.setLocalDescription(desc);
-  console.log("Ответ успешно создан! Теперь устанавливаем одно и то же описание в качестве Локального на локальном пире и удаленного на пире 1", desc.sdp);
-  socket.emit('set_description', JSON.stringify({'description': desc}))
+function consume() {
+  let producerId = document.getElementById('producer-input').value
+  socket.emit('consume-stream', { producerId: producerId, rtpCapabilities: rtpCapabilities});
 }
 
-
-function OnErrorCreationAnswer(error) {
-console.log('Error creation answer!', error);
-}
-
-
-var answerConstraints = { 
-'mandatory': { 'OfferToReceiveAudio':true, 'OfferToReceiveVideo':true } 
-};
-
-function onAddCandidateToLocalPeer(event) {
-    if (event.candidate) {
-      console.log("На локальном пире установлен кандидат. Вызов удаленного пира 1 для установки этого же кандидата.", event.candidate.candidate);
-      socket.emit('ice_candidates', JSON.stringify({'candidate': event.candidate}))
-    }
-  }
-
-
-
-function onAddRemoteStreamToLocalPeer(event) {
-    console.log('Видео с пира 1 было добавлено на пир 2');
-    console.log(remoteVideo);
-    console.log(event.stream);
-    remoteVideo.srcObject = event.stream;
-    console.log(remoteVideo.srcObject);
-}
-
-function onReceivedOffer(desc) {
-    console.log("pc2_receiveOffer()", desc);
-    // Создаем объект RTCPeerConnection для второго участника аналогично первому
-   localPeer = new RTCPeerConnection(servers);
-   localPeer.onicecandidate = onAddCandidateToLocalPeer; // Задаем обработчик события при появлении ICE-кандидата
-   localPeer.onaddstream = onAddRemoteStreamToLocalPeer; // При появлении потока подключим его к HTML <video>
-    // Теперь, когда второй RTCPeerConnection готов, передадим ему полученный Offer SDP (первому мы передавали локальный поток)
-    console.log('Создан второй пир. Устанавливается удаленное описание переданное из первого пира.')
-    console.log('Offer: ', desc)
-   localPeer.setRemoteDescription(desc);
-    // Запросим у второго соединения формирование данных для сообщения Answer
-   localPeer.createAnswer( 
-      OnSuccessAnswerCreation,
-      OnErrorCreationAnswer,
-      answerConstraints
-    );
-  }
-
-socket.on('created_offer', (arg) => {
-   let desc_json = JSON.parse(arg)
-   let desc = desc_json.offer
-   onReceivedOffer(desc)
+socket.on('consumer-created', ({ id, kind, rtpParameters }) => {
+  const consumer = new RTCRtpReceiver(rtpParameters);
+  document.querySelector('video#remote-video').srcObject = consumer.track;
 });
 
-socket.on('ice_candidates', (arg) => {
-  let arg_json = JSON.parse(arg)
-  let arg_data = arg_json.candidate
-  localPeer.addIceCandidate(new RTCIceCandidate(arg_data))
-  console.log('Получено сообщение из сокета. Добавлен кандидат из него на пир.', arg_data)
-});
+$('#button').click(consume)
+
+
+
